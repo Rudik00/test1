0eb962dfb6047b91e6e89cd0abee8f96ba4c575b
Rudik00 <ilarudyj78@gmail.com>
2026-03-04 13:01:55 +0400

    change save location

--- app/saver.py
+++ app/saver.py
@@ -1,19 +1,28 @@
-import logging
+from pathlib import Path
 from bs4 import BeautifulSoup
+
+import logging
 
 
 def save_pretty_html(response: requests.Response, filename="html.txt") -> None:
     """
-    Принимает response от requests.get() и сохраняет HTML в файл красиво.
+    Сохраняет HTML в папку reports/ в красивом виде.
     """
-    # Форматируем HTML с отступами
     logging.info("Formatting HTML")
+
     soup = BeautifulSoup(response.text, "html.parser")
     pretty_html = soup.prettify()
+
     logging.info("HTML formatted successfully")
-    
-    # Сохраняем в файл
-    with open(filename, "w", encoding="utf-8") as f:
+
+    # Путь к папке reports (рядом с app/)
+    base_dir = Path(__file__).resolve().parent.parent
+    reports_dir = base_dir / "reports"
+    reports_dir.mkdir(exist_ok=True)  # создаст папку если её нет
+
+    file_path = reports_dir / filename
+
+    with open(file_path, "w", encoding="utf-8") as f:
         f.write(pretty_html)
 
-    logging.info(f"HTML saved to {filename}")
+    logging.info(f"HTML saved to {file_path}")
